c66977c81b585db94aa4ef191f358aca43104bee
Jaime Alberto Silva <jsilva@gluky.co>
2021-03-12 07:51:50 -0500

    Led handling.

--- keyboards/ergodone/keymaps/jaimealsilva/keymap.c
+++ keyboards/ergodone/keymaps/jaimealsilva/keymap.c
@@ -1,13 +1,53 @@
 #include "version.h"
-
+#include "led.h"
 #include "jaimealsilva.c"
+
+#define LEDC_TOP 20
+#define LEDC_S 1
+uint8_t ledc[2];
+
+/* false: Caps Lock LED is off
+   true: Caps Lock LED is on */
+bool CAPS_LED = false;
 
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
-    ergodox_board_led_on();
-    ergodox_right_led_3_on();
+    ergodox_led_all_off();
+    ledc[0] = 0;
+    ledc[1] = 0;
 };
 
+void drive_led(uint8_t l) {
+    uint8_t i = l - 1;
+    if (ledc[i] < LEDC_S) {
+        ergodox_right_led_on(l);
+    } else {
+        ergodox_right_led_off(l);
+    }
+    ++ledc[i];
+    if (ledc[i] >= LEDC_TOP) {
+        ledc[i] = 0;
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+      case KC_CAPS:
+        if (record->event.pressed) {
+          // Turn LED1 On/Off for Caps Lock
+          if (CAPS_LED) {
+            ergodox_right_led_3_off();
+            CAPS_LED = false;
+          } else {
+            ergodox_right_led_3_on();
+            CAPS_LED = true;
+          }
+        }
+        return true;
+        break;
+    }
+    return true;
+};
 
 // Runs constantly in the background, in a loop.
 void matrix_scan_user(void) {
@@ -17,12 +57,11 @@ void matrix_scan_user(void) {
     ergodox_right_led_1_off();
     ergodox_right_led_2_off();
     switch (layer) {
-      // TODO: Make this relevant to the ErgoDox EZ.
         case 1:
-            ergodox_right_led_1_on();
+            drive_led(1);
             break;
         case 2:
-            ergodox_right_led_2_on();
+            drive_led(2);
             break;
         default:
             // none
@@ -30,3 +69,12 @@ void matrix_scan_user(void) {
     }
 
 };
+
+/* bool led_update_user(led_t led_state) { */
+/*     if (led_state.num_lock) { */
+/*         ergodox_right_led_3_on(); */
+/*     } else { */
+/*         ergodox_right_led_3_off(); */
+/*     } */
+/*     return true; */
+/* } */
