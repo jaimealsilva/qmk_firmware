1405fc69e32147daf2698a26211a9ac7adcf8edf
Jaime Alberto Silva <jaime.silva@gluky.com>
2023-04-23 16:18:56 -0500

    Cambiado TAPPING_TOGGLE a 2

---
+++ keyboards/ktec/ergodone/keymaps/jaimealsilva/config.h
@@ -0,0 +1,8 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#pragma once
+
+#undef  TAPPING_TOGGLE
+#define TAPPING_TOGGLE 2
